5a170023c16b6f4878e45ec9dd7e47103bf5969a
Mohan Chandra <mohan.chandra@oracle.com>
2017-06-12 12:18:34 +0530

    Adding Code for CORS issues

--- src/main/java/com/docs/portal/configuration/security/DocsPortalSecurityCofiguration.java
+++ src/main/java/com/docs/portal/configuration/security/DocsPortalSecurityCofiguration.java
@@ -5,6 +5,7 @@
  */
 package com.docs.portal.configuration.security;
 
+import com.docs.portal.cors.filter.DocsCORSFilter;
 /**
  *
  * @author mohchand
@@ -18,6 +19,9 @@ import com.docs.portal.security.filter.TokenAuthenticationFilter;
 import com.docs.portal.service.impl.AuthenticateDocsPortalServiceImpl;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,6 +34,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.session.SessionManagementFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -45,6 +53,22 @@ public class DocsPortalSecurityCofiguration extends WebSecurityConfigurerAdapter
     public DocsPortalLogoutHandler jwtLogoutHandler() {
         return new DocsPortalLogoutHandler();
     }
+    
+//    @Bean
+//    DocsCORSFilter corsFilter() {
+//    	DocsCORSFilter filter = new DocsCORSFilter();
+//        return filter;
+//    }
+    
+//    @Bean
+//	CorsConfigurationSource corsConfigurationSource() {
+//		CorsConfiguration configuration = new CorsConfiguration();
+//		configuration.setAllowedOrigins(Arrays.asList("http://localhost:8000"));
+//		configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", configuration);
+//		return source;
+//	}
     @Autowired
     AuthenticateDocsPortalServiceImpl jwtUserDetailsService;
 
@@ -76,13 +100,15 @@ public class DocsPortalSecurityCofiguration extends WebSecurityConfigurerAdapter
         http
                 //                .csrf()
                 //                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+//        		.addFilterBefore(corsFilter(), SessionManagementFilter.class)
+//        		.cors().and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint)
                 .and()
                 .addFilterBefore(jwtAuthenticationTokenFilter(), BasicAuthenticationFilter.class)
                 .authorizeRequests()
-                .antMatchers("/login", "/forgotPasswordService/**")
+                .antMatchers("/forgotPasswordService/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
--- src/main/java/com/docs/portal/cors/filter/DocsCORSFilter.java
+++ src/main/java/com/docs/portal/cors/filter/DocsCORSFilter.java
@@ -2,41 +2,79 @@ package com.docs.portal.cors.filter;
 
 import java.io.IOException;
 
+import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.core.Ordered;
 
-public class DocsCORSFilter extends OncePerRequestFilter {
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class DocsCORSFilter implements Filter {
 	private static final Logger LOGGER = LoggerFactory.getLogger(DocsCORSFilter.class);
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-		response.addHeader("Access-Control-Allow-Origin", "*");
-
-		// if (request.getHeader("Access-Control-Request-Method") != null &&
-		// "OPTIONS".equals(request.getMethod())) {
-		logger.trace("Sending Header....");
-		// CORS "pre-flight" request
-		response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-		// response.addHeader("Access-Control-Allow-Headers", "Authorization");
-		response.addHeader("Access-Control-Allow-Headers", "Content-Type, Portal-Type, Authorization, Accept");
-		// response.addHeader("Access-Control-Allow-Headers", "Portal-Type");
-		response.addHeader("Access-Control-Max-Age", "1");
-		// }
-		if (request.getMethod().equals("OPTIONS")) {
-			response.setStatus(HttpServletResponse.SC_OK);
-			return;
-		}
-
-		filterChain.doFilter(request, response);
-
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
 	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8000");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Portal-Type, Authorization, Accept");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if(!"OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            chain.doFilter(req, res);
+        }
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+//	@Override
+//	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+//			throws ServletException, IOException {
+//		response.addHeader("Access-Control-Allow-Origin", "*");
+//
+//		// if (request.getHeader("Access-Control-Request-Method") != null &&
+//		// "OPTIONS".equals(request.getMethod())) {
+//		logger.trace("Sending Header....");
+//		// CORS "pre-flight" request
+//		response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//		// response.addHeader("Access-Control-Allow-Headers", "Authorization");
+//		response.addHeader("Access-Control-Allow-Headers", "Content-Type, Portal-Type, Authorization, Accept");
+//		// response.addHeader("Access-Control-Allow-Headers", "Portal-Type");
+//		response.addHeader("Access-Control-Max-Age", "1");
+//		// }
+//		if (request.getMethod().equals("OPTIONS")) {
+//			response.setStatus(HttpServletResponse.SC_OK);
+//			return;
+//		}
+//
+//		filterChain.doFilter(request, response);
+//
+//	}
 
 }
